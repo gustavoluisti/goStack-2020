046d3eeb3c75f80a35eed86d3ec86cf08da8a070
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-08-07 00:48:13 -0300

    Atualizando App

--- api-node-typescript/src/server.ts
+++ api-node-typescript/src/server.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
@@ -16,22 +16,23 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-    if(err instanceof AppError) {
-        return response.status(err.statusCode).json({
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                status: 'error',
+                message: err.message,
+            });
+        }
+
+        console.error(err);
+
+        return response.status(500).json({
             status: 'error',
-            message: err.message,
-        })
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
-    })
-});
-
+            message: 'Internal server error',
+        });
+    },
+);
 
 app.listen(3333, () => {
     console.log('🚀 Server started on port 3333');
